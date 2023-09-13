cd3fce7f871a49bfc4eeb41c4386468f59b16399
lenovo <mmdhazem93@gmail.com>
2023-09-14 01:43:47 +0300

    initial project. added empty home screen.

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String RouteName = "RouteName";
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:islami/home/home_screen.dart';
+
+void  main (){
+runApp(MyApp());
+
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.RouteName ,
+      routes: {
+        HomeScreen.RouteName: (context) => HomeScreen(),
+
+      }
+      );
+  }
+
+}
